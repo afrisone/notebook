f872ac508af1dfc3c9005e9e757294b8f167f2bb
Alicia Frisone <a.frisone54@gmail.com>
2023-03-23 19:10:53 -0600

    added express boilerplate

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log('Hello World')
+import express, { Express } from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
